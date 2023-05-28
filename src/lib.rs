3a2b25cde197ed10595d3177c40aac62b4448aab
Daniel Brai <braidaniel2001@gmail.com>
2023-05-28 22:32:23 +0100

    feat(library): started impl for hyperseek crawling modes

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,32 @@ pub struct HyperSeek {
     max_depth: u32,
     mode: HyperSeekMode,
 }
+
+impl HyperSeek {
+    pub fn create(seed_urls: Vec<String>, max_depth: u32, mode: HyperSeekMode) -> HyperSeek {
+        let visited_urls = Arc::new(Mutex::new(HashSet::new()));
+        let urls_to_download = Arc::new(Mutex::new(VecDeque::from(seed_urls)));
+
+        HyperSeek {
+            visited_urls,
+            urls_to_download,
+            max_depth,
+            mode,
+        }
+    }
+
+    pub fn crawl(&mut self) {
+        match self.mode {
+            HyperSeekMode::Sync => self.crawl_sync(),
+            HyperSeekMode::Async => self.crawl_async(),
+        }
+    }
+
+    fn crawl_sync(&mut self) {
+        // implement hyperseek web crawling synchronously
+    }
+
+    fn crawl_async(&mut self) {
+        // implement hyperseek web crawling asynchronously
+    }
+}
