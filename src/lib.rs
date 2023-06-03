b74d27c5e34711ece073aa4118617f9d30062a2c
Daniel Brai <braidaniel2001@gmail.com>
2023-06-03 01:09:00 +0100

    feat(library): implemented sync web crawling

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ use tokio::task::spawn;
 use url::Url;
 
 pub enum HyperSeekMode {
-    Sync,
     Async,
+    Sync,
 }
 
 pub struct HyperSeek {
@@ -33,13 +33,66 @@ impl HyperSeek {
 
     pub fn crawl(&mut self) {
         match self.mode {
-            HyperSeekMode::Sync => self.crawl_sync(),
             HyperSeekMode::Async => self.crawl_async(),
+            HyperSeekMode::Sync => self.crawl_sync(),
         }
+    }
+
+    fn get_next_url(&mut self) -> Option<String> {
+        self.urls_to_download.lock().unwrap().pop_front()
+    }
+
+    async fn download_page_async(
+        &self,
+        client: &AsyncClient,
+        url: &str,
+    ) -> Result<String, reqwest::Error> {
+        client.get(url).send().await?.text().await
+    }
+
+    fn download_page_sync(&self, client: &SyncClient, url: &str) -> Result<String, reqwest::Error> {
+        client.get(url).send()?.text()
+    }
+
+    fn get_url_depth(&self, url: &str) -> u32 {
+        let parsed_url = Url::parse(url).expect("Failed to parse URL");
+        parsed_url
+            .path_segments()
+            .map(|segments| segments.count() as u32)
+            .unwrap_or(0)
+    }
+
+    fn extract_urls(&self, page_content: &str) -> Vec<String> {
+        Document::from(page_content)
+            .find(Name("a"))
+            .filter_map(|n| n.attr("href"))
+            .map(|href| href.to_string())
+            .collect()
     }
 
     fn crawl_sync(&mut self) {
         // implement hyperseek web crawling synchronously
+        let client = SyncClient::new();
+        let mut urls_to_add = Vec::new();
+
+        while let Some(url) = self.get_next_url() {
+            if let Ok(page_content) = self.download_page_sync(&client, &url) {
+                let extracted_urls = self.extract_urls(&page_content);
+
+                self.visited_urls.lock().unwrap().insert(url.clone());
+
+                for extracted_url in extracted_urls {
+                    let depth = self.get_url_depth(&url) + 1;
+                    if !self.visited_urls.lock().unwrap().contains(&extracted_url)
+                        && depth <= self.max_depth
+                    {
+                        urls_to_add.push(extracted_url);
+                    }
+                }
+            }
+        }
+
+        self.urls_to_download.lock().unwrap().extend(urls_to_add);
     }
 
     fn crawl_async(&mut self) {
